0294432bed02e4223f37fa5665a6952b45fe1bff
kamilzachara <kamil.zachara98@gmail.com>
2022-04-06 01:33:49 +0200

    Task details view works

--- Yet Another To-Do list/TaskViewController.swift
+++ Yet Another To-Do list/TaskViewController.swift
@@ -8,11 +8,39 @@
 import UIKit
 
 class TaskViewController: UIViewController {
+    
+    @IBOutlet var label: UILabel!
+    var task: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        label.text=task
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "delete", style: .done, target: self, action: #selector(deleteTask))
+    
+    }
+    
+    @objc func deleteTask(){
+//        let newCount = count - 1
+//
+//        UserDefaults.standard.set(newCount , forKey: "count")
+//        UserDefaults.standard.set(nil , forKey: "task_\(currentPosition)")
+//
+//        tasks.removeAll()
+//
+//        guard let count = UserDefaults.standard.value(forKey: "count") as? Int else{
+//            return
+//        }
+//
+//        for x in 0..<count{
+//            if let task = UserDefaults.standard.value(forKey: "task_\(x+1)") as? String{
+//                tasks.append(task)
+//            }
+//        }
+//
+//        tableView.reloadData()
+//
     }
     
 
